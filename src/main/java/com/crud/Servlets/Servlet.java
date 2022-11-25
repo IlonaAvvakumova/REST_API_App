518fe66ea69b9e200d2f24ff5613a413373c1cb3
Ilona <avvakumova_ilona@mail.ru>
2022-11-25 14:08:39 +0300

    new

--- src/main/java/com/crud/Servlets/Servlet.java
+++ src/main/java/com/crud/Servlets/Servlet.java
@@ -1,39 +1,78 @@
 package com.crud.Servlets;
+
 import com.crud.model.Label;
 import com.crud.service.LabelService;
-import javax.servlet.ServletException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet(urlPatterns = "/label")
 public class Servlet extends HttpServlet {
     //ObjectMapper получить JSON
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         PrintWriter printWriter = resp.getWriter();
         Object param = req.getParameter("param");
-        String param2 = req.getParameter("param2");
-        System.out.println(param.toString());
+        req.getParameter("getAll");
         LabelService labelService = new LabelService();
-        Label byId = labelService.getById(Integer.parseInt(param.toString()));
-        printWriter.write("<html>");
-        printWriter.write("<body>");
-        printWriter.format("<p>privet %s</p>", byId);
-        printWriter.format(param2 + " two parametr");
-        printWriter.write("</body>");
-        printWriter.write("</html>");
 
+        //показать по ИД
+
+        if (req.getQueryString().equals("param=" + param)) {
+            Label byId = labelService.getById(Integer.parseInt(param.toString()));
+            printWriter.write("<html>");
+            printWriter.write("<body>");
+            printWriter.format("<p>privet %s</p>", byId);
+            printWriter.write("</body>");
+            printWriter.write("</html>");
+            return;
+        }
+        log("Log info");
+        //показать Все
+        if (req.getQueryString().equals("getAll")) {
+            List<Label> labels = labelService.getAll();
+            printWriter.format(labels.toString());
+            return;
+        }
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         PrintWriter printWriter = resp.getWriter();
-
         printWriter.write("poka");
+        LabelService labelService = new LabelService();
+        Label label = new Label();
+        Label label1 = new Label();
 
+        String createLabel = req.getParameter("createName");
+        if (req.getQueryString().equals("createName=" + createLabel)) {
+            label.setName(createLabel);
+            labelService.create(label);
+            return;
+        }
+
+        String updateLabel = req.getParameter("updateName");
+        String updateLabel2 = req.getParameter("updateId");
+
+        if (req.getQueryString().equals("updateName=" + updateLabel + "&updateId=" + updateLabel2) ||
+                req.getQueryString().equals("updateId=" + updateLabel2 + "&updateName=" + updateLabel)) {
+            label1.setId(Integer.parseInt(updateLabel2));
+            label1.setName(updateLabel);
+            System.out.println(label1);
+            labelService.update(label1);
+           return;
+        }
+
+        String d = req.getParameter("deleteId");
+        Integer deleteId = Integer.parseInt(d);
+        if (req.getQueryString().equals("deleteId=" + deleteId)) {
+            labelService.deleteById(deleteId);
+            return;
+        }
     }
 }
